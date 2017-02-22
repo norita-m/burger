d0c912f09ffc4ddcb5a9c9a586e825455cbd1ab0
Nora Mata <norita@Noras-MacBook-Pro.local>
2017-02-21 21:31:05 -0600

    adding final HW files

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE burgers_db;
 CREATE TABLE burgers (
 	id INT NOT NULL AUTO_INCREMENT,
 	burger_name VARCHAR(35) NOT NULL,
-	devoured BOOLEAN NOT NULL,
+	devoured BOOLEAN DEFAULT false,
 	date TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
